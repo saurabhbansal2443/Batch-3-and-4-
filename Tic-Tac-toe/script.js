22897738c36daab40556b0d6667d614e196b62ca
Saurabh Bansal <saurabhbansal2443@gmail.com>
2023-12-14 19:55:55 +0530

    tic tac toe

--- Tic-Tac-toe/script.js
+++ Tic-Tac-toe/script.js
@@ -7,12 +7,14 @@ let allCells = document.querySelectorAll(".cell");
 
 let gamePlay = false;
 let turn = "X";
+let count = 0 ;
 
 startBtn.addEventListener("click", function () {
 
     if (gamePlay == false) {
         gameStm.innerHTML = "Player X's turn "
         startBtn.innerHTML = "Reset";
+        turn = "X";
     } else {
         gameStm.innerHTML = "";
         startBtn.innerHTML = "Start"
@@ -29,20 +31,105 @@ startBtn.addEventListener("click", function () {
 
 gameBoard.addEventListener("click", function (event) {
 
-    if (gamePlay == true) {
+    if (gamePlay == true && event.target.innerHTML == "") {
 
         let myCell = event.target;
+        count++;
 
         if (turn == "X") {
             myCell.innerHTML = turn;
+
             turn = "0";
-            
+
         } else {
             myCell.innerHTML = turn;
+            myCell.style.color = 'red';
             turn = "X";
         }
-        gameStm.innerHTML=`Player ${turn}'s turn `
+        gameStm.innerHTML = `Player ${turn}'s turn `
+
+          let output = checkWinner();
+          if(output == 2){
+            gameStm.innerHTML = "Player X wins the Game";
+            clearGrid();
+          }else if ( output == 1){
+            gameStm.innerHTML = "Player 0 wins the Game";
+            clearGrid();
+          }else if ( count == 9 ){
+            gameStm.innerHTML = "TIE";
+            clearGrid();
+          }
     } else {
 
     }
 })
+
+function checkWinner() {
+    console.log("check win");
+
+    if (
+        (allCells[0].innerHTML == "X" &&
+            allCells[1].innerHTML == "X" &&
+            allCells[2].innerHTML == "X") ||
+        (allCells[3].innerHTML == "X" &&
+            allCells[4].innerHTML == "X" &&
+            allCells[5].innerHTML == "X") ||
+        (allCells[6].innerHTML == "X" &&
+            allCells[7].innerHTML == "X" &&
+            allCells[8].innerHTML == "X") ||
+        (allCells[0].innerHTML == "X" &&
+            allCells[3].innerHTML == "X" &&
+            allCells[6].innerHTML == "X") ||
+        (allCells[1].innerHTML == "X" &&
+            allCells[4].innerHTML == "X" &&
+            allCells[7].innerHTML == "X") ||
+        (allCells[2].innerHTML == "X" &&
+            allCells[5].innerHTML == "X" &&
+            allCells[8].innerHTML == "X") ||
+        (allCells[0].innerHTML == "X" &&
+            allCells[4].innerHTML == "X" &&
+            allCells[8].innerHTML == "X") ||
+        (allCells[2].innerHTML == "X" &&
+            allCells[4].innerHTML == "X" &&
+            allCells[6].innerHTML == "X")
+    ) {
+        return 2;
+    } else if (
+        (allCells[0].innerHTML == "0" &&
+            allCells[1].innerHTML == "0" &&
+            allCells[2].innerHTML == "0") ||
+        (allCells[3].innerHTML == "0" &&
+            allCells[4].innerHTML == "0" &&
+            allCells[5].innerHTML == "0") ||
+        (allCells[6].innerHTML == "0" &&
+            allCells[7].innerHTML == "0" &&
+            allCells[8].innerHTML == "0") ||
+        (allCells[0].innerHTML == "0" &&
+            allCells[3].innerHTML == "0" &&
+            allCells[6].innerHTML == "0") ||
+        (allCells[1].innerHTML == "0" &&
+            allCells[4].innerHTML == "0" &&
+            allCells[7].innerHTML == "0") ||
+        (allCells[2].innerHTML == "0" &&
+            allCells[5].innerHTML == "0" &&
+            allCells[8].innerHTML == "0") ||
+        (allCells[0].innerHTML == "0" &&
+            allCells[4].innerHTML == "0" &&
+            allCells[8].innerHTML == "0") ||
+        (allCells[2].innerHTML == "0" &&
+            allCells[4].innerHTML == "0" &&
+            allCells[6].innerHTML == "0")
+    ) {
+        return 1;
+    } else {
+        return 0;
+    }
+
+}
+
+
+function clearGrid(){
+    for(let i = 0 ; i<allCells.length ; i++){
+        allCells[i].innerHTML = "";
+    }
+}
