1a8931b6aca5831896fae33fe7d34c51d0d4ca06
Saurabh Bansal <saurabhbansal2443@gmail.com>
2023-12-14 19:00:46 +0530

    grid clicked

--- Tic-Tac-toe/script.js
+++ Tic-Tac-toe/script.js
@@ -5,16 +5,17 @@ let allCells = document.querySelectorAll(".cell");
 
 // console.log(allCells);
 
-let gamePlay = false ;
+let gamePlay = false;
+let turn = "X";
 
-startBtn.addEventListener("click",function(){
+startBtn.addEventListener("click", function () {
 
-    if(gamePlay == false ){
-        gameStm.innerHTML="Player X's turn "
-    startBtn.innerHTML="Reset";
-    }else{
-        gameStm.innerHTML="";
-        startBtn.innerHTML="Start"
+    if (gamePlay == false) {
+        gameStm.innerHTML = "Player X's turn "
+        startBtn.innerHTML = "Reset";
+    } else {
+        gameStm.innerHTML = "";
+        startBtn.innerHTML = "Start"
     }
 
     gamePlay = !gamePlay;
@@ -26,6 +27,22 @@ startBtn.addEventListener("click",function(){
 //     })
 // }
 
-gameBoard.addEventListener("click",function(event){
-    console.log(event.target);
+gameBoard.addEventListener("click", function (event) {
+
+    if (gamePlay == true) {
+
+        let myCell = event.target;
+
+        if (turn == "X") {
+            myCell.innerHTML = turn;
+            turn = "0";
+            
+        } else {
+            myCell.innerHTML = turn;
+            turn = "X";
+        }
+        gameStm.innerHTML=`Player ${turn}'s turn `
+    } else {
+
+    }
 })
